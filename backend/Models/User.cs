2a697ad1ea11cc8927efeb20b30f3b7288b989ff
Anya-gh <Aniket.Singh@warwick.ac.uk>
2024-06-13 12:38:26 +0100

    Renamed entities to models

--- backend/Models/User.cs
+++ backend/Models/User.cs
@@ -2,6 +2,6 @@ public class User {
   public int ID { get; set; }
   public string Username { get; set; } = "";
   public string Password { get; set; } = "";
-  public ICollection<Record> Records { get; set; }
-  public UserStats UserStats { get; set; }
+  public required ICollection<Record> Records { get; set; }
+  public required UserStats UserStats { get; set; }
 }
