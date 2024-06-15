405e57557ff5057f466a21b49b38ecafddb4259d
Anya-gh <Aniket.Singh@warwick.ac.uk>
2024-06-15 17:00:22 +0100

    changed beginner and intermediate
    beginner now more exploitable, intermediate more challenging

--- backend/Handlers/BotHandler.cs
+++ backend/Handlers/BotHandler.cs
@@ -1,5 +1,3 @@
-using System.Diagnostics;
-
 public interface IBotHandler {
 
 }
@@ -22,37 +20,55 @@ public class BotHandler {
 public abstract class IBot {
   public virtual string Play(List<Match> matches) { return "rock"; }
   public List<string> Choices = new List<string>() { "rock", "paper", "scissors" };
+  public Random random = new Random();
   public Dictionary<string, string> WinOutcomes = new Dictionary<string, string>() {
     {"rock", "paper"},
     {"paper", "scissors"},
     {"scissors", "rock"}
   };
 } 
-
+/*
+Plays whatever won last time. Treats draw as a win.
+If there hasn't been a match yet, picks a choice at random.
+*/
 public class BeginnerBot : IBot {
   public override string Play(List<Match> matches) {
 
-    var match = matches.LastOrDefault();
-    if (match != null) {
-      // Winning outcome vs. whatever they played last time
-      return WinOutcomes[match.PlayerChoice];
+    var lastMatch = matches.LastOrDefault();
+    if (lastMatch != null) {
+      if (lastMatch.Result == "win") { return lastMatch.PlayerChoice; }
+      else { return lastMatch.BotChoice; }
     }
     else {
-      return "rock";
+      return Choices[random.Next(0, 2)];
     }
   }
 }
-
+/*
+Based on this paper: https://arxiv.org/pdf/1404.5199v1
+If it just lost: switches to the choice that beats what the player just played.
+If it just won: switches to the losing choice that the player just played.
+If there hasn't been a match yet, picks a choice at random.
+*/
 public class IntermediateBot : IBot {
   public override string Play(List<Match> matches) {
-    List<string> lastFewChoices = matches.TakeLast(Math.Min(matches.Count(), 3)).Select(match => match.PlayerChoice).ToList();
-    if (lastFewChoices.Count() > 0) {
-      // Winning outcome vs. whatever you played the most the last three times
-      string mostCommonChoice = lastFewChoices.GroupBy(c => c).OrderByDescending(g => g.Count()).Select(c => c.Key).First();
-      return WinOutcomes[mostCommonChoice]; 
+    var lastMatch = matches.LastOrDefault();
+    if (lastMatch != null) {
+      // Player won
+      if (lastMatch.Result == "win") {
+        return WinOutcomes[lastMatch.PlayerChoice];
+      }
+      // Draw
+      else if (lastMatch.Result == "draw") {
+        return Choices[random.Next(0, 2)];
+      }
+      // Bot won
+      else {
+        return lastMatch.PlayerChoice;
+      }
     }
-    else { 
-      return "paper";
+    else {
+      return Choices[random.Next(0, 2)];
     }
   }
 }
