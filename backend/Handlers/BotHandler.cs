cb68af840b3bfe3bcfaf0410ed7cce76666c489e
Anya-gh <Aniket.Singh@warwick.ac.uk>
2024-06-15 16:30:44 +0100

    implemented intermediate bot

--- backend/Handlers/BotHandler.cs
+++ backend/Handlers/BotHandler.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 
 public interface IBotHandler {
 
@@ -10,31 +11,48 @@ public class BotHandler {
     switch(levelID) {
       case 1:
         return new BeginnerBot();
+      case 2:
+        return new IntermediateBot(); 
       default:
         return null;
     }
   }
 }
 
-public interface IBot {
-  public string Play(List<Match> matches);
+public abstract class IBot {
+  public virtual string Play(List<Match> matches) { return "rock"; }
+  public List<string> Choices = new List<string>() { "rock", "paper", "scissors" };
+  public Dictionary<string, string> WinOutcomes = new Dictionary<string, string>() {
+    {"rock", "paper"},
+    {"paper", "scissors"},
+    {"scissors", "rock"}
+  };
 } 
 
 public class BeginnerBot : IBot {
-  public string Play(List<Match> matches) {
-    var winOutcomes = new Dictionary<string, string>() {
-      {"rock", "paper"},
-      {"paper", "scissors"},
-      {"scissors", "rock"}
-    };
+  public override string Play(List<Match> matches) {
 
     var match = matches.LastOrDefault();
     if (match != null) {
-      // Winning outcomes vs. whatever they played last time
-      return winOutcomes[match.PlayerChoice];
+      // Winning outcome vs. whatever they played last time
+      return WinOutcomes[match.PlayerChoice];
     }
     else {
       return "rock";
+    }
+  }
+}
+
+public class IntermediateBot : IBot {
+  public override string Play(List<Match> matches) {
+    List<string> lastFewChoices = matches.TakeLast(Math.Min(matches.Count(), 3)).Select(match => match.PlayerChoice).ToList();
+    if (lastFewChoices.Count() > 0) {
+      // Winning outcome vs. whatever you played the most the last three times
+      string mostCommonChoice = lastFewChoices.GroupBy(c => c).OrderByDescending(g => g.Count()).Select(c => c.Key).First();
+      return WinOutcomes[mostCommonChoice]; 
+    }
+    else { 
+      return "paper";
     }
   }
 }
