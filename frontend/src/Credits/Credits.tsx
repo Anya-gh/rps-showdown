96e157c188e1962630e5dd00dd2d9c7e6516a83d
Anya-gh <Aniket.Singh@warwick.ac.uk>
2024-06-17 14:06:27 +0100

    updated credits

--- frontend/src/Credits/Credits.tsx
+++ frontend/src/Credits/Credits.tsx
@@ -4,9 +4,13 @@ export default function Credits() {
   return (
     <div className="flex flex-col items-center px-20 py-20 w-screen">
       <Header />
-      <h1 className="text-2xl font-bold mt-10 text-left mb-2">Credits</h1>
+      <h1 className="text-2xl font-bold mt-10 text-left mb-1">Credits</h1>
+      <p className="text-xs mb-4">Any items that are not credited here were created by me.</p>
       <ul className="list-disc">
         <li><a className="text-blue-400" href="https://www.flaticon.com/free-icons/rock-paper-scissors" title="rock paper scissors icons">Rock paper scissors icons created by Freepik - Flaticon</a></li>
+        <li><a className="text-blue-400" target="_blank" href="https://icons8.com/icon/RlXIngfmfHJq/statistic">Statistic</a> icon by <a className="text-blue-400" target="_blank" href="https://icons8.com">Icons8</a></li>
+        <li><a className="text-blue-400" href="https://www.flaticon.com/free-icons/speedometer" title="speedometer icons">Speedometer icons created by Freepik - Flaticon</a></li>
+        <li><a className="text-blue-400" href="https://www.flaticon.com/free-icons/eye" title="eye icons">Eye icons created by Gregor Cresnar - Flaticon</a></li>
       </ul>
     </div>
   )
