fefd4acf166d09fbfabba51c6b6b9657162e9acf
Anya-gh <Aniket.Singh@warwick.ac.uk>
2024-06-18 01:06:46 +0100

    reset db

--- backend/Migrations/20240618000633_InitialCreate.Designer.cs
+++ backend/Migrations/20240618000633_InitialCreate.Designer.cs
@@ -10,7 +10,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace backend.Migrations
 {
     [DbContext(typeof(RPSDbContext))]
-    [Migration("20240617232144_InitialCreate")]
+    [Migration("20240618000633_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
